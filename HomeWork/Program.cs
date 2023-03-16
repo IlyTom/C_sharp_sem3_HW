043090d9651fe5bd41ca8aca9938875d07db8571
IlyTom <ilytoma@mail.ru>
2023-03-16 21:29:14 +0300

    Work2

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,38 +1,37 @@
-﻿// Задача 1. Напишите программу, которая принимает на вход пятизначное число
-//  и проверяет, является ли оно палиндромом.
-// 14212 -> нет
-// 12821 -> да
-// 23432 -> да
+﻿// Напишите программу, которая принимает на вход координаты двух точек и
+// находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
-string ReadString()
+double[] PointPosition(int numPoint)
 {
-    Console.WriteLine("Введите пятизначное число: ");
-    string txt = Console.ReadLine()!;
-    while (true)
-    {
-        if (txt.Length > 4 && txt.Length < 6)
-        {
-            return txt;
-        }
-        else
-        {
-            Console.WriteLine("Введено не пятизначное число, повторите ввод числа: ");
-            txt = Console.ReadLine()!;
-        }
-    }
+    Console.Write($"Введите X координату {numPoint} точки: ");
+    double x = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write($"Введите Y координату {numPoint} точки: ");
+    double y = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write($"Введите Z координату {numPoint} точки: ");
+
+    double z = Convert.ToDouble(Console.ReadLine());
+    double[] PointXYZ = new double[] { x, y, z };
+
+    Console.WriteLine($"Координаты точки {numPoint} : {PointXYZ[0]}, {PointXYZ[1]}, {PointXYZ[2]}");
+
+    return PointXYZ;
 }
 
-void CheckPalindrome(string str)
+double CheckDistance(double[] one, double[] two)
 {
-    if (str[0] == str[4] && str[1] == str[3])
-    {
-        Console.WriteLine($"Число {str} является палиндромом.");
-    }
-    else
-    {
-        Console.WriteLine($"Число {str} не является палиндромом.");
-    }
+    double DistX = Math.Pow(one[0] - two[0], 2);
+    double DistY = Math.Pow(one[1] - two[1], 2);
+    double DistZ = Math.Pow(one[2] - two[2], 2);
+    double Distance = Math.Sqrt(DistX + DistY + DistZ);
+    return Distance;
 }
 
-string number = ReadString();
-CheckPalindrome(number);
+double[] PointA = PointPosition(1);
+double[] PointB = PointPosition(2);
+
+double Distance = CheckDistance(PointA, PointB);
+Console.WriteLine($"Расстояние между точками A({PointA[0]},{PointA[1]},{PointA[2]}) и B({PointB[0]},{PointB[1]},{PointB[2]}) равно {Math.Round(Distance,2)}");
