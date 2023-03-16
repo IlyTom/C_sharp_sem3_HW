1f39e58c1db7ee4eaecf5671fdbf2fca3e665002
IlyTom <ilytoma@mail.ru>
2023-03-16 21:56:44 +0300

    Work3

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,38 +1,21 @@
-﻿// Задача 1. Напишите программу, которая принимает на вход пятизначное число
-//  и проверяет, является ли оно палиндромом.
-// 14212 -> нет
-// 12821 -> да
-// 23432 -> да
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-string ReadString()
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int ReadInt()
 {
-    Console.WriteLine("Введите пятизначное число: ");
-    string txt = Console.ReadLine()!;
-    while (true)
+    Console.Write("Введите число N:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PowCube(int n)
+{
+    for (int i = 1; i <= n; i++)
     {
-        if (txt.Length > 4 && txt.Length < 6)
-        {
-            return txt;
-        }
-        else
-        {
-            Console.WriteLine("Введено не пятизначное число, повторите ввод числа: ");
-            txt = Console.ReadLine()!;
-        }
+            Console.Write($"{Math.Pow(i,3)} ");
     }
 }
 
-void CheckPalindrome(string str)
-{
-    if (str[0] == str[4] && str[1] == str[3])
-    {
-        Console.WriteLine($"Число {str} является палиндромом.");
-    }
-    else
-    {
-        Console.WriteLine($"Число {str} не является палиндромом.");
-    }
-}
-
-string number = ReadString();
-CheckPalindrome(number);
+int N = ReadInt();
+PowCube(N);
